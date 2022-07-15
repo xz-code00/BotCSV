4ad4b52a6a8ff3515fada0645e5da6902b913aa7
Sergio <47334042+xz-code00@users.noreply.github.com>
2022-07-15 16:57:50 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,16 @@
-FILE_CSV_INPUT = "c.txt"
+from re import S
+
+
+FILE_CSV_INPUT = "vs_20220714_NON-CONTABILE.txt"
 FILE_CSV_OUTPUT = "new.txt"
 
 
 #Apertura del file csv
 f = open(FILE_CSV_INPUT,"r") 
 
+with open(FILE_CSV_OUTPUT, "a") as text_file:
+    text_file.write("data,ora,cli,numero,area geografica,durata,importo,nng,vis_cli,prefix")
+    text_file.write("\n")
 
 #Ciclo eseguito per ogni riga
 while True:
@@ -51,6 +57,7 @@ while True:
     
 
 #Viene estratto per essere comparato il numero da modificare
+    x = 0
     while(True):
         
         if(lineCopy[y] == '"'):
@@ -60,50 +67,54 @@ while True:
 
         y += 1
 
-    
 
 #Generazione e se necessario sostituzione della nuova numerazione
-    a = 1
+    a = 0
 
     lenLine = len(numero)
-    if(numeroCambiare not in numero):
-        if (numero[0] == '0' and numero[1] == '0'):
-            numeroNuovo += '00'
-        numeroNuovo += numeroCambiare
-        lenN = len(numeroCambiare)
 
-        while(a < lenN):
+    if(numero[1] != '0'):
+        numeroNuovo += "0039"
+        leN = len(numeroCambiare)
+        
+        while(a < lenLine):
             try:
-                numeroNuovo += numero[ lenLine + a]
+                numeroNuovo += numero[a]
             except:
                 print("In esecuzione")   
 
             a += 1
-    else:
-        numeroNuovo = numero
-    #print(numeroCambiare)
 
+    else:
+        numeroNuovo += numero
 
 
 #Viene generata la parte finale della stringa e accodata
     lineMod += numeroNuovo
 
-    b = len(lineMod)
+    s = 0
+    l = 0
+    while(s < 11):
+        if(lineCopy[l] == '"'):
+            s += 1
+        l += 1
+    
     c = 0
 
-    while(c < 13):
-        lineMod = lineMod + lineCopy[b]
+    while(c < 17):
+        try:
+            lineMod = lineMod + lineCopy[s]
         
-        if (lineMod[b] == '"'):
+            if (lineMod[s] == '"'):
+                c += 1
+            s += 1
+        except:
             c += 1
-        b += 1
-
-    #print (lineMod)
 
 
 #Viene inserita la nuova stringa nel file
     with open(FILE_CSV_OUTPUT, "a") as text_file:
         text_file.write(lineMod)
-        text_file.write("\n")
+    
 
     print("Riga generata con successo")
